260e51a72de35f25ce3031b48d09c2811193f804
rise42 <makedonskiy2500@gmail.com>
2017-11-12 11:10:44 +0200

    Branch fixes + API url corrections

--- src/config.js
+++ src/config.js
@@ -2,17 +2,15 @@ export default {
   coindesk: {
     get: {
       url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
-      timestamp: ['time', 'updated'],
-      price: ['bpi', 'USD', 'rate'],
-      timeformat: 'mills'
+      timestamp: ['time', 'updatedISO'],
+      price: ['bpi', 'USD', 'rate']
     }
   },
   coinmarketcap: {
     get: {
       url: 'https://api.coinmarketcap.com/v1/ticker/bitcoin/',
       timestamp: [0, 'last_updated'],
-      price: [0, 'price_usd'],
-      timeformat: 'unix'
+      price: [0, 'price_usd']
     }
   }
 }
