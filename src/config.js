30ba5c503e4d150f7cc38a3c6ea01a0d2ccc59f0
rise42 <makedonskiy2500@gmail.com>
2017-11-12 11:07:38 +0200

    Timeformat added

--- src/config.js
+++ src/config.js
@@ -3,14 +3,16 @@ export default {
     get: {
       url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
       timestamp: ['time', 'updated'],
-      price: ['bpi', 'USD', 'rate']
+      price: ['bpi', 'USD', 'rate'],
+      timeformat: 'mills'
     }
   },
   coinmarketcap: {
     get: {
       url: 'https://api.coinmarketcap.com/v1/ticker/bitcoin/',
       timestamp: [0, 'last_updated'],
-      price: [0, 'price_usd']
+      price: [0, 'price_usd'],
+      timeformat: 'unix'
     }
   }
 }
